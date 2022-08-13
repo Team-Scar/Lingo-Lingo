0a09106a5ba300d610d570a08ff8097886da51a2
Symphon-y <travisredden@outlook.com>
2022-08-13 16:32:16 -0500

    added global scss file, sidebar scss file, and global scss variables

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
+import './sidebar.scss';
+
+import '../../global.scss';
+
 const Sidebar = () => {
   return (
     <div>
-      <h1>Sidebar</h1>
+      <h1 className='header'>Sidebar</h1>
+
       <BrowserRouter>
         <nav>
           <Link to='/'>Home</Link> | <Link to='/livechat'>Chat</Link> |{' '}
