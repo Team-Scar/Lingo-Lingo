c0089b7fe32f2d18a34b93ea18515003b4b131ba
Fahad Syed <syed.216@gmail.com>
2022-08-13 17:34:55 -0500

    Started working on userprofile

---
+++ client/src/components/userprofile/UserProfile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {useNavigate, useParams } from 'react-router-dom';
+
+const UserProfile = () => {
+  let navigate = useNavigate();
+  let {username} = useParams();
+  if (username = current user) {
+    return (
+  // return
+        //userprofile
+        //edit botton
+          //onclick
+            //form
+            //submit
+              //onclick
+                //axios.put
+            //cancel
+         //return to previous page
+      )
+    } return (
+    //else
+      //return
+        //userprofle
+        //add connection button
+          //axios.put
+        //message button
+          //react-route to messages
+    )
+}
+
+export default UserProfile;
