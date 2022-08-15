55dadea38f6869f535ae4e30a037d5a28fb6790a
vpatel89 <virenpatel89@gmail.com>
2022-08-15 18:19:21 -0400

    added firebaseConfig file

---
+++ client/src/firebaseConf.js
@@ -0,0 +1,8 @@
+export const config = {
+  FIREBASE_API_KEY: 'AIzaSyDvBSe_IxPMMDIh1Ckrwna9ucmQ89iD3CU',
+  FIREBASE_AUTH_DOMAIN: 'lingolingo-dev.firebaseapp.com',
+  FIREBASE_PROJECT_ID: 'lingolingo-dev',
+  FIREBASE_STORAGE_BUCKET: 'lingolingo-dev.appspot.com',
+  FIREBASE_MESSAGING_SENDER_ID: '264965553788',
+  FIREBASE_APP_ID: '1:264965553788:web:035988fccea23e918293f9',
+};
