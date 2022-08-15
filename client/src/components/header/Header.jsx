504689b4085553f7892bcb1cda72134aab35590d
Symphon-y <travisredden@outlook.com>
2022-08-14 20:13:08 -0500

    Added Icons from Mockup, Added skeleton to header and sidebar, added basic styling

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import './header.scss';
+import '../../global.scss'
 
 const Header = () => {
-  return <h1>Header</h1>;
+  // TODO: headerTitle should update automatically based on sidebar nav link;
+  let headerTitle = 'All Lingo Boards';
+
+  return (
+    <div className='header'>
+      <h1 className='header_title'>{headerTitle}</h1>
+      <ul className='header_filter_container'>
+        {
+          // TODO: li 'clear' should clear filters in state;
+        }
+        <li className='header_filter' val='clear'>Home</li>
+        {
+          // TODO: li's should update based on user onboarding selections;
+        }
+        <li className='header_filter' val='/events'>Temp Filter</li>
+      </ul>
+    </div>);
 };
 
 export default Header;
--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import LogoFull from '../../../assets/LogoFull.svg';
 
+// Icons
+import HomeSVG from '../../../assets/sidebar_icons/Home_Icon.svg';
+import ChatSVG from '../../../assets/sidebar_icons/Chat_Icon.svg';
+
+// Styles
 import './sidebar.scss';
-
 import '../../global.scss';
 
 const Sidebar = () => {
   return (
-    <div>
-      <h1 className='header'>Sidebar</h1>
-
+    <div className='sidebar'>
+      <img className='lingo_logo' src={LogoFull} alt="Lingo Logo" />
       <BrowserRouter>
-        <nav>
-          <Link to='/'>Home</Link> | <Link to='/livechat'>Chat</Link> |{' '}
-          <Link to='/events'>Events</Link>|
+        <nav className='sidebar_navigation'>
+          <div>
+            <img className='home_icon' src={HomeSVG} alt="home icon" />
+            <Link to='/'>Home</Link>
+          </div>
+          <div>
+            <img className='chat_icon' src={ChatSVG} alt="chat icon" />
+            <Link to='/livechat'>Chat</Link>
+          </div>
+          <Link to='/events'>Events</Link>
+          <Link to='/connections'>Connections</Link>
+          <Link to='/events'>Lingo Boards</Link>
         </nav>
       </BrowserRouter>
     </div>
