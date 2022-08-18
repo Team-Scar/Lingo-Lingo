dfd3135c6eac7fd1830a79bd809e78c6252b1133
David Kroll <dkroll713@gmail.com>
2022-08-18 17:12:34 -0500

    power is about to go out

--- client/src/components/forum/Cards.jsx
+++ client/src/components/forum/Cards.jsx
@@ -10,6 +10,8 @@ import {AiFillCaretDown} from 'react-icons/ai';
 import {MdOutlineComment} from 'react-icons/md';
 import {RiShareLine} from 'react-icons/ri';
 
+import comment_icon from '../../../assets/forum_icons/comment_Icon.svg';
+
 import forumStore from './_forumState.js';
 
 
@@ -46,45 +48,87 @@ function Cards(props) {
     navigate('/discussions');
   };
 
-  return (
-    <div className="card">
-      <div className="content">
-        {/* <img src={post.photo}></img> */}
-        <img
-          className="image"
-          src={post.photo}
-          // style={{height: '100px', width: '100px'}}
-        />
-        <h3 className="cardTitle">{post.title}</h3>
-        <p className="cardContent">{post.content}</p>
-      </div>
-      <div className="info">
-        <h5><TimeAgo date={post.timestamp} /> by <em>{post.username}</em></h5>
-        <h5>Lingo: <em>{post.language_name}</em></h5>
-        <h5>Jargon: <em>{post.jargon_name}</em></h5>
-      </div>
-      <div className="interaction">
-        <div className="votes">
-          <AiFillCaretUp
-            className='caretUp'
-            onClick={upvote}
+  if (post.photo) {
+    return (
+      <div className="photoCard">
+        <div className="content">
+          {/* <img src={post.photo}></img> */}
+          <img
+            className="image"
+            src={post.photo}
+            // style={{height: '100px', width: '100px'}}
           />
-          <AiFillCaretDown
-            className='caretDown'
-            onClick={downvote}
-          />
-          <p id="vote">{post.vote}</p>
+          <h3 className="cardTitle">{post.title}</h3>
+          <p className="cardContent">{post.content}</p>
         </div>
-        <div className="comments" id={post.title} onClick={showDetails} name={post.id} >
-          <MdOutlineComment className="commentIcon" />
-          <p>{post.responses}</p>
+        <div className="info">
+          <h5><TimeAgo date={post.timestamp} /> by <em>{post.username}</em></h5>
+          <h5>Lingo: <em>{post.language_name}</em></h5>
+          <h5>Jargon: <em>{post.jargon_name}</em></h5>
         </div>
-        <div className="share">
-          <RiShareLine className="shareIcon" />
+        <div className="interaction">
+          <div className="votes">
+            <AiFillCaretUp
+              className='caretUp'
+              onClick={upvote}
+            />
+            <AiFillCaretDown
+              className='caretDown'
+              onClick={downvote}
+            />
+            <p id="vote">{post.vote}</p>
+          </div>
+          <div className="comments" id={post.title} onClick={showDetails} name={post.id} >
+            <img className="commentIcon" src={comment_icon} />
+            <p>{post.responses}</p>
+          </div>
+          <div className="share">
+            <RiShareLine className="shareIcon" />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="card">
+        <div className="content">
+          {/* <img src={post.photo}></img> */}
+          <img
+            className="image"
+            src={post.photo}
+            // style={{height: '100px', width: '100px'}}
+          />
+          <h3 className="cardTitle">{post.title}</h3>
+          <p className="cardContent">{post.content}</p>
+        </div>
+        <div className="info">
+          <h5><TimeAgo date={post.timestamp} /> by <em>{post.username}</em></h5>
+          <h5>Lingo: <em>{post.language_name}</em></h5>
+          <h5>Jargon: <em>{post.jargon_name}</em></h5>
+        </div>
+        <div className="interaction">
+          <div className="votes">
+            <AiFillCaretUp
+              className='caretUp'
+              onClick={upvote}
+            />
+            <AiFillCaretDown
+              className='caretDown'
+              onClick={downvote}
+            />
+            <p id="vote">{post.vote}</p>
+          </div>
+          <div className="comments" id={post.title} onClick={showDetails} name={post.id} >
+            <img className="commentIcon" src={comment_icon} />
+            <p>{post.responses}</p>
+          </div>
+          <div className="share">
+            <RiShareLine className="shareIcon" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Cards;
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,9 +3,9 @@ const router = require('express').Router();
 const {checkIfAuthenticated} = require('../middleware.js');
 
 router.post('/signup', controllers.userAuth.signUpPost);
-router.post('/create-account', checkIfAuthenticated, controllers.userAuth.createAccount);
-router.get('/allLanguages', checkIfAuthenticated, controllers.userAuth.getAllLanguages);
-router.get('/allJargons', checkIfAuthenticated, controllers.userAuth.getAllJargons);
-router.get('/getUserId', checkIfAuthenticated, controllers.userAuth.getUserId);
+router.post('/create-account', controllers.userAuth.createAccount);
+router.get('/allLanguages', controllers.userAuth.getAllLanguages);
+router.get('/allJargons', controllers.userAuth.getAllJargons);
+router.get('/getUserId', controllers.userAuth.getUserId);
 
 module.exports = router;
