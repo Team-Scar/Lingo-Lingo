4c40c83d54897947fde745c3e4f42b3c835afa3c
vpatel89 <virenpatel89@gmail.com>
2022-08-18 16:20:02 -0400

    working on responses

--- client/src/components/forum_details/Forum_details.jsx
+++ client/src/components/forum_details/Forum_details.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-
-// import Sidebar from '../sidebar/Sidebar.jsx';
 import './forumDetails.scss';
 import ForumMainPost from './Forum_mainpost.jsx';
 import ForumThread from './Forum_thread.jsx';
-// import forumStore from '../forum/_forumState.js';
+// import globalStore from './../zustand.js';
 
 const ForumDetails = () => {
-  // const posts = forumStore((state) => state.posts);
   return (
     <div className="forumDetails">
       <ForumMainPost />
--- client/src/components/forum_details/Forum_thread.jsx
+++ client/src/components/forum_details/Forum_thread.jsx
@@ -1,33 +1,14 @@
 import React from 'react';
 
 import postStore from './_postState.js';
+import globalStore from './../zustand.js';
 import ForumMessage from './Forum_message.jsx';
 
 const ForumThread = (props) => {
   const responsesData = postStore((state) => state.responsesData);
+  const userId = globalStore((state) => state.userId);
+  const userName = globalStore((state) => state.userName);
 
-  // const responses = [
-  //   {
-  //     id: 1,
-  //     response_to_id: '',
-  //     content: 'TEXT TEXT TEXT TEXT TEXT',
-  //     photo: 'https://picsum.photos/id/237/200/300',
-  //     timestamp: '08/16/2022 11:11:11',
-  //     vote: 5,
-  //     user_id: 2,
-  //     post_id: 5,
-  //   },
-  //   {
-  //     id: 2,
-  //     response_to_id: 1,
-  //     content: 'MORE TEXT MORE TEXT MORE TEXT MORE TEXT MORE TEXT',
-  //     photo: 'https://picsum.photos/id/1/200/300',
-  //     timestamp: '08/16/2022 11:15:11',
-  //     vote: 1,
-  //     user_id: 3,
-  //     post_id: 5,
-  //   },
-  // ];
 
   return (
     <div className="forumThread">
