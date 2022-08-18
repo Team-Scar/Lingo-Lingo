e512f705d5f5370a9e83e190d0df6bab7caa0372
Symphon-y <travisredden@outlook.com>
2022-08-18 17:05:34 -0500

    added navigation button

---
+++ client/src/components/Nav_btn/NavBtn.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import globalStore from '../../zustand.js';
+import '../../global.scss';
+import plusIcon from '../../../assets/PlusIcon.svg';
+import {useNavigate} from 'react-router-dom';
+
+const NavBtn = (props) => {
+  const navigate = useNavigate();
+  const style = props.style || 'mfn_btn';
+
+  const path = props.path || ('/');
+
+  return (
+    <button className={style} onClick={(e) => {
+      navigate(path);
+    }}>{icon}</button>
+  );
+};
+
+export default NavBtn;
