97171ad82cee6da8b569b03fd034bc05aacc4755
David Fan <davidlfan@gmail.com>
2022-08-18 23:34:40 -0400

    in the middle of messing with the webpack config

--- client/src/components/livechat/LiveChat.jsx
+++ client/src/components/livechat/LiveChat.jsx
@@ -5,17 +5,20 @@ import './livechat.scss';
 import {useLocalStorage} from './hooks/useLocalStorage.js';
 import {ContactsProvider} from './contexts/ContactsProvider.jsx';
 import {ConversationsProvider} from './contexts/ConversationsProvider.jsx';
+import {SocketProvider} from './contexts/SocketProvider.jsx';
 
 
 const LiveChat = () => {
   const [id, setId] = useLocalStorage('id');
 
   const messagebox = (
-    <ContactsProvider>
-      <ConversationsProvider id={id}>
-        <Messages id={id}/>
-      </ConversationsProvider>
-    </ContactsProvider>
+    <SocketProvider id={id}>
+      <ContactsProvider>
+        <ConversationsProvider id={id}>
+          <Messages id={id}/>
+        </ConversationsProvider>
+      </ContactsProvider>
+    </SocketProvider>
   );
   return (
     <div>
--- server/index.js
+++ server/index.js
@@ -92,6 +92,26 @@ app.get('*', (req, res) => {
 });
 
 /* ==== Server Binding === */
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
+
+/* ==== Socket Binding === */
+const io = require('socket.io')(server);
+
+io.on('connection', (socket) => {
+  const id = socket.handshake.query.id;
+  socket.join(id);
+
+  socket.on('send-message', ({recipients, text}) => {
+    recipients.forEach((recipient) => {
+      const newRecipients = recipients.filter((r) => r !== recipient);
+      newRecipients.push(id);
+      socket.broadcast.to(recipient).emit('receive-message', {
+        recipients: newRecipients, sender: id, text,
+      });
+    });
+  });
+});
+
+module.exports = server;
