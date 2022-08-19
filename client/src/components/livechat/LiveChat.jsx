d497f74a592b68a7bd53ed37da4efccb440a73b9
David Fan <davidlfan@gmail.com>
2022-08-19 10:50:22 -0400

    Stashing the changes for pull in case something breaks before demo

--- client/src/components/livechat/LiveChat.jsx
+++ client/src/components/livechat/LiveChat.jsx
@@ -6,16 +6,20 @@ import {useLocalStorage} from './hooks/useLocalStorage.js';
 import {ContactsProvider} from './contexts/ContactsProvider.jsx';
 import {ConversationsProvider} from './contexts/ConversationsProvider.jsx';
 import {SocketProvider} from './contexts/SocketProvider.jsx';
+import globalStore from '../../zustand.js';
+
 
 
 const LiveChat = () => {
   const [id, setId] = useLocalStorage('id');
+  const userId = globalStore((state) => state.userId);
+  const userName = globalStore((state) => state.userName);
 
   const messagebox = (
-    <SocketProvider id={id}>
+    <SocketProvider id={id} userName={userName}>
       <ContactsProvider>
-        <ConversationsProvider id={id}>
-          <Messages id={id}/>
+        <ConversationsProvider id={id} userName={userName}>
+          <Messages id={id} userName={userName}/>
         </ConversationsProvider>
       </ContactsProvider>
     </SocketProvider>
